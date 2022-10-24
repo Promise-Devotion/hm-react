ac44c0f1836276702bdc731fca3ce54eedf87369
Promise-Devotion <sijun0224@msn.com>
2022-10-24 22:05:22 +0800

    canvas画图

--- src/pages/Charts/ChartsComponent/canvasComponent.jsx
+++ src/pages/Charts/ChartsComponent/canvasComponent.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { useEffect } from "react";
 export default function CanvasConponent(params) {
   const canvasRef = React.useRef();
+  const draw = () => {
+    var canvas = document.getElementById("tutorial");
+    if (!canvas.getContext) return;
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "rgb(200,0,0)";
+    //绘制矩形
+    ctx.fillRect(10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(30, 30, 55, 50);
+  };
+  useEffect(() => {
+    draw();
+  }, []);
   return (
     <canvas
+      id="tutorial"
       ref={canvasRef}
       width={window.innerWidth - 200}
       height={window.innerHeight - 400}
