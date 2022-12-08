0bc083d1195da4ef8f2e4ec27e5ea95fa85ebb2e
Promise-Devotion <sijun0224@msn.com>
2022-12-08 18:36:05 +0800

    删除代码

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,28 @@ import { Navigate, Route, Routes } from "react-router-dom";
 // import { routes useRoutes} from "@/router";
 import Navbar from "./components/Navbar";
 import Header from "./components/Top/index";
-import Home from "./pages/Home/Home";
-import Todolist from "./pages/Todolist/Todolist";
 import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
-import Register from "./pages/register";
-import ZustandComponent from "./pages/Zustand";
-import LearnComponent from "./pages/LearnComponent";
 import Charts from "./pages/Charts";
-import LineChart from "./pages/Charts/ChartsComponent/lineChart";
 import CanvasComponent from "./pages/Charts/ChartsComponent/canvasComponent";
+import LineChart from "./pages/Charts/ChartsComponent/lineChart";
+import Contact from "./pages/Contact";
 import Basic from "./pages/Home/BasicKnowledge/Basic/Basic";
+import EventMechanism from "./pages/Home/BasicKnowledge/Basic/Eventmechanism";
 import ComponentCommunication from "./pages/Home/BasicKnowledge/ComponentCommunication";
 import ChildToParent from "./pages/Home/BasicKnowledge/ComponentCommunication/ChildToParent";
 import Descendant from "./pages/Home/BasicKnowledge/ComponentCommunication/descendant";
 import NonRealtionalComponent from "./pages/Home/BasicKnowledge/ComponentCommunication/Non-relational";
 import ParentToChild from "./pages/Home/BasicKnowledge/ComponentCommunication/ParentToChild";
 import Sibling from "./pages/Home/BasicKnowledge/ComponentCommunication/sibling";
-import EventMechanism from "./pages/Home/BasicKnowledge/Basic/Eventmechanism";
-import NotFound from "./pages/NotFound";
-import Swrpage from "./pages/Swrpage";
+import Home from "./pages/Home/Home";
 import HttpDemo from "./pages/Http/index";
+import LearnComponent from "./pages/LearnComponent";
+import NoPage from "./pages/NoPage";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/register";
+import Swrpage from "./pages/Swrpage";
+import Todolist from "./pages/Todolist/Todolist";
+import ZustandComponent from "./pages/Zustand";
 
 export default function App() {
   return (
@@ -66,14 +66,6 @@ export default function App() {
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-      {/* <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="home" element={<Home />}></Route>
-            <Route path="*" element={<NotFound />} />
-            <Route path="basic" element={Basic}></Route>
-            <Route path="component" element={<LearnComponent />} />
-        </Route>
-      </Routes> */}
     </div>
   );
 }
